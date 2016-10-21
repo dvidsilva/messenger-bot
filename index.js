0a390c088499f40a6eca0228df2b14dfa91c10e1
David Silva <contacto@dvidsilva.com>
2016-10-21 16:35:46 -0700

    using ENV vars when available

--- index.js
+++ index.js
@@ -20,52 +20,53 @@ app.get('/', function (req, res, next) {
 app.get('/webhook/', handleVerify);
 app.post('/webhook/', receiveMessage);
 
-function handleVerify(req, res, next){
-	if (req.query['hub.verify_token'] === conf.VERIFY_TOKEN) {
-    return res.send(req.query['hub.challenge']);
-  }
-  res.send('Validation failed, Verify token mismatch');
-}
-
-function receiveMessage(req, res, next){
-	var message_instances = req.body.entry[0].messaging;
-	message_instances.forEach(function(instance){
-		var sender = instance.sender.id;
-		if(instance.message && instance.message.text) {
-			var msg_text = instance.message.text;
-			sendMessage(sender, msg_text, true);
-		}
-	});
-  res.sendStatus(200);
-}
-
-function sendMessage(receiver, data, isText){
-	var payload = {};
-	payload = data;
-	
-	if(isText) {
-		payload = {
-			text: data
-		}
-	}
-
-	request({
-    url: conf.FB_MESSAGE_URL,
-    method: 'POST',
-    qs: {
-    	access_token: conf.PROFILE_TOKEN
-    },
-    json: {
-      recipient: {id: receiver},
-      message: payload
+function handleVerify(req, res, next) {
+    var token = process.env.VERIFY_TOKEN || conf.VERIFY_TOKEN;
+    if (req.query['hub.verify_token'] === token) {
+        return res.send(req.query['hub.challenge']);
     }
-  }, function (error, response) {
-  	if(error) console.log('Error sending message: ', error);
-  	if(response.body.error) console.log('Error: ', response.body.error);
-  });
+    res.send('Validation failed, Verify token mismatch');
+}
+
+function receiveMessage(req, res, next) {
+    var message_instances = req.body.entry[0].messaging;
+    message_instances.forEach(function(instance){
+        var sender = instance.sender.id;
+        if(instance.message && instance.message.text) {
+            var msg_text = instance.message.text;
+            sendMessage(sender, msg_text, true);
+        }
+    });
+    res.sendStatus(200);
+}
+
+function sendMessage(receiver, data, isText) {
+    var payload = {};
+    payload = data;
+
+    if(isText) {
+        payload = {
+            text: data
+        };
+    }
+
+    request({
+        url: conf.FB_MESSAGE_URL,
+        method: 'POST',
+        qs: {
+            access_token: process.env.PROFILE_TOKEN || conf.PROFILE_TOKEN
+        },
+        json: {
+            recipient: {id: receiver},
+            message: payload
+        }
+    }, function (error, response) {
+        if(error) console.log('Error sending message: ', error);
+        if(response.body.error) console.log('Error: ', response.body.error);
+    });
 }
 
 var port = conf.PORT;
 httpServer.listen(port, function () {
-	console.log("Express http server listening on port " + port);
+    console.log("Express http server listening on port " + port);
 });
